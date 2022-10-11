da16908e025efdabd4800fdff608033f95ed9590
user07 <user07>
2022-10-11 15:57:52 +0300

    firs6t commit

---
+++ практические/src/main/kotlin/Main.kt
@@ -0,0 +1,82 @@
+import kotlin.math.PI
+import kotlin.math.sqrt
+
+fun main() {
+    //3
+    var age = (18.0 + 30.0)/2
+    println(age)
+    exercisesSolved +=1
+ //1
+    val exercises  = 13
+    println(exercises)
+    exercisesSolved +=1
+    var exercisesSolved = 6
+    println(exercisesSolved)
+    exercisesSolved +=1
+    //2
+    val myAge = 18
+    println(myAge)
+    exercisesSolved +=1
+    //4
+    val testNumber =43
+    val evenOdd = testNumber/2.0
+    println(evenOdd)
+    exercisesSolved +=1
+
+    //5
+    var answer = ((0+1)+10)*10
+    println(answer)
+    exercisesSolved +=1
+    answer =110 shr 3
+    println(answer)
+    exercisesSolved +=1
+    //7
+    val a:Int=46
+    val b:Int=10
+    val answer1=(a*100)+b
+    println(answer1)
+    exercisesSolved +=1
+    val answer2=(a*100)+(b*100)
+    println(answer2)
+    exercisesSolved +=1
+    val answer3=(a*100)+(b/10)
+    println(answer3)
+    exercisesSolved +=1
+    //8
+    val answer4=5*3-4/(2*2)
+    println(answer4)
+    exercisesSolved +=1
+    //9
+    val d=13.0
+    val c=65.0
+    val average=(d+c)/2
+    println(average)
+    exercisesSolved +=1
+    val fahrenheit:Double
+        val F = 87.0
+        val celcius=F/1.8-32
+    println(celcius)
+    exercisesSolved +=1
+    //13
+    val x1=3.0
+    val y1=-5.0
+    val x2=7.0
+    val y2=-3.0
+    val distance = sqrt((x2-x1).pow(2)+((y2-y1).pow(2))
+    println(distance)
+    exercisesSolved +=1
+    //12
+    val degrees=26.0
+    val radians= degrees * PI / 180
+    println(radians)
+    exercisesSolved +=1
+    //11
+    val position=27
+    val row=position/8
+    println(row)
+    exercisesSolved +=1
+        val column=position/8
+    println(column)
+    exercisesSolved +=1
+    //6
+}
